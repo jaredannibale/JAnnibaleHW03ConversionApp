acdc0d05647b9b59b4ab18bd4ad3bb279efa0979
Jared Annibale <jaredannibale@gmail.com>
2017-02-04 12:53:23 -0500

    125 Million App

---
+++ The $125 Million App/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  The $125 Million App
+//
+//  Created by Jared Annibale on 2/3/17.
+//  Copyright © 2017 Jared Annibale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var userInput: UITextField!
+
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func convertButtonPressed(_ sender: UIButton) {
+        
+        if let miles = Double(userInput.text!) {
+            let km = miles * 1.6
+            resultsLabel.text = "\(miles) miles = \(km) kilometers."
+        } else {
+            resultsLabel.text = ""
+            
+            let alertController = UIAlertController(title: "Entry Error", message: "Please be sure to only enter valid numbers. No empty string, commas, symbols, or non-numeric characters.", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+            
+            
+        }
+
+   
+}
+}
+
